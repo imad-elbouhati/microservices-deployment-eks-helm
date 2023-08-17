f0bbf9beba2b2461850434b8da71a91f1f149500
imad <elbouhatiimad@gmail.com>
2023-08-17 16:18:44 +0100

    terraform: create gitlab iam user and its credentials

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,5 +3,14 @@ output "eks_cluster_endpoint" {
 }
 
 output "eks_cluster_certificate_authority" {
-  value = aws_eks_cluster.aws_eks.certificate_authority 
+  value = aws_eks_cluster.aws_eks.certificate_authority
+}
+
+output "gitlab-access-key" {
+  value = aws_iam_access_key.gitlab.id
+}
+
+output "gitlab-secret-key" {
+  value = aws_iam_access_key.gitlab.secret
+  sensitive = true
 }
